f5ce9c574939a7ad88bb6b0086aa614ae34eaac3
Arthur Estevan Vargas <arthur.estevan01@gmail.com>
2022-06-03 11:20:34 -0300

    Initial commit

---
+++ src/br/com/entra21/main/Main.java
@@ -0,0 +1,38 @@
+package br.com.entra21.main;
+
+import java.util.Scanner;
+
+public class Main {
+
+    static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        byte option;
+
+        do {
+
+            System.out.println("Select Exercise");
+            option = input.nextByte();
+
+            switch (option) {
+
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        } while (option != 0);
+
+    }
+
+}

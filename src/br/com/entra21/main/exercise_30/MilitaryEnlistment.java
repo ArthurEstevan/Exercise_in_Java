6222e21b34df0830b3205e77b645d7a9aab341c0
Arthur Estevan Vargas <arthur.estevan01@gmail.com>
2022-06-23 13:17:59 -0300

    Exercise | 23-06-22

--- src/br/com/entra21/main/exercise_30/MilitaryEnlistment.java
+++ src/br/com/entra21/main/exercise_30/MilitaryEnlistment.java
@@ -1,10 +1,5 @@
 package br.com.entra21.main.exercise_30;
 
-import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeParseException;
-import java.util.Date;
 import java.util.Scanner;
 
 public class MilitaryEnlistment {
@@ -15,7 +10,30 @@ public class MilitaryEnlistment {
                 "Your program should also show the time remaining or overdue.\n");
 
         Scanner input = new Scanner(System.in);
-        String name, date;
+
+        int age;
+        short currentYear, yearBorn;
+
+        System.out.print("Enter the year you were born: ");
+        yearBorn = input.nextShort();
+
+        System.out.print("Enter current year: ");
+        currentYear = input.nextShort();
+
+        age = currentYear - yearBorn;
+
+        if (age<18 && age>-1) {
+            System.out.println("\nIt's not yet time for your enlistment.");
+            System.out.println("You have "+(18-age)+" years left for you to enlist.\n");
+        } else if (age==18) {
+            System.out.println("\nYou're 18, it's time to enlist in the military.\n");
+        } else if (age>18) {
+            System.out.println("\nYou should have already completed your enlistment.");
+            System.out.println((age-18)+" year(s) has passed.");
+        } else {
+            System.out.println("\nOur! Are you from the future?");
+        }
+
 
 
     }
